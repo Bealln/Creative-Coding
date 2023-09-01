592ad2779e6989d56c5e4256bd79557d35253730
Beatriz López Neira <beatrizlopezlopez-neira@Beatrizs-MacBook-Pro-2.local>
2023-09-01 21:21:26 +0100

    A few changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Very cool project <code>src/App.js</code> and save to reload.
+            Very cool projeeeect <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
